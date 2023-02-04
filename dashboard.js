e7ba2036267ece98da2fddca64fcd2ec384daf3b
avanibhute02 <89659503+avanibhute02@users.noreply.github.com>
2023-02-04 14:55:43 -0600

    l

--- dashboard.js
+++ dashboard.js
@@ -1,26 +1,26 @@
-import {PythonShell} from 'python-shell';
+// import {PythonShell} from 'python-shell';
 
-document.getElementById("summary").onclick = async () => {
-    //document.body.append("refresh attempted" + document.createElement('br'));
-    //savedtext.append("refresh attempted");
-        document.body.append(document.createElement('br'));
-        //chrome.storage.local.get(['key'], function(result) {
-        //for(var ii = 0; ii < result.key.length; ii++){
-            //document.body.append('New highlight:    "');
-            //document.body.append(result.key[ii]);
-            //document.body.append('"');
-            //alert('ooooooooooo');
-        //}
+// document.getElementById("summary").onclick = async () => {
+//     //document.body.append("refresh attempted" + document.createElement('br'));
+//     //savedtext.append("refresh attempted");
+//         document.body.append(document.createElement('br'));
+//         //chrome.storage.local.get(['key'], function(result) {
+//         //for(var ii = 0; ii < result.key.length; ii++){
+//             //document.body.append('New highlight:    "');
+//             //document.body.append(result.key[ii]);
+//             //document.body.append('"');
+//             //alert('ooooooooooo');
+//         //}
 
-        PythonShell.runString(
-            'from tkinter import *;Tk().mainloop()',
-            null,
-            function (err){
-                if(err) throw err;
-                console.log('finished');
-            }
-        );
+//         PythonShell.runString(
+//             'from tkinter import *;Tk().mainloop()',
+//             null,
+//             function (err){
+//                 if(err) throw err;
+//                 console.log('finished');
+//             }
+//         );
         
-        document.body.append(document.createElement('br'));
+//         document.body.append(document.createElement('br'));
     
-}
+// }
